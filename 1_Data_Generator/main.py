f74c97113ed17fa2ee704329bd30c470b4c37a59
ReedGraff <Rangergraff@gmail.com>
2023-04-23 20:55:54 -0400

    updates

--- 1_Data_Generator/main.py
+++ 1_Data_Generator/main.py
@@ -1,14 +1,32 @@
 import tensorflow as tf
-from tensorflow import image_dataset_from_directory
 import matplotlib.pyplot as plt
 
-def BasicNN():
-    # Create a basic model instance
+def get_basic_model():
+    # increase input size...
     model = tf.keras.Sequential([
-        tf.keras.layers.Flatten(input_shape=(28, 28)),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(32, activation='relu'),
-        tf.keras.layers.Dense(10)
+        tf.keras.layers.Resizing(28, 28),
+
+        tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu', input_shape=(28, 28, 3)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Dropout(0.25),
+    
+        tf.keras.layers.Conv2D(128, (3, 3), padding='same', activation='relu'),
+        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Dropout(0.25),
+    
+        tf.keras.layers.Conv2D(256, (2, 2), padding='same', activation='relu'),
+        tf.keras.layers.Conv2D(256, (2, 2), activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Dropout(0.25),
+    
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(5)
     ])
 
     # Compile the model
@@ -23,15 +41,15 @@ def initial_generator(batch_size, ):
     yield 0
 
 def main():
-    model = BasicNN()
-    history = model.fit(image_dataset_from_directory("../Data"), epochs=1)
+    model = get_basic_model()
+    history = model.fit(tf.keras.utils.image_dataset_from_directory("../Data/Monkeys/Training Data", crop_to_aspect_ratio=True), epochs=128)
 
     # plot history
     plt.plot(history.history['accuracy'], label='accuracy')
     plt.plot(history.history['loss'], label = 'loss')
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
-    plt.ylim([0.5, 1])
+    plt.ylim([0, 1])
     plt.legend(loc='lower right')
     plt.savefig("history.png")
 
---
+++ Data/Monkeys/README.md
@@ -0,0 +1,4 @@
+Monkey dataset credits:
+* Modified dataset
+
+@misc{deepnets_2022, title={10 species of Monkey | Multi-Class Classification}, url={https://www.kaggle.com/datasets/utkarshsaxenadn/10-species-of-monkey-multiclass-classification}, journal={Kaggle.com}, author={DeepNets}, year={2022} }
